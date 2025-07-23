d2f24d24829c874b7dd6485d2873e6792993566e
rubina-cat <l760729@gmail.com>
2025-07-24 01:35:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from openai import OpenAI
-import yaml
+import os
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
